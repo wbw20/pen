569f51fd97b15d6349f25799a412c393cb92829d
William B. Wettersten <wbw20@case.edu>
2014-08-07 10:04:31 -0700

    WIP modularize

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -1,48 +1,90 @@
-Pen.prototype.toolbar = function() {
+Pen.prototype.toolbar = {
+  init: function() {
+    var that = this, icons = '';
 
-  var that = this, icons = '';
+    _.map(this.config.list, function(item) {
+      var klass = 'pen-icon icon-' + item;
+      icons += '<i class="' + klass + '" data-action="' + item + '">' + (item.match(/^h[1-6]|p$/i) ? item.toUpperCase() : '') + '</i>';
+      if((item === 'createlink')) icons += '<input class="pen-input" placeholder="http://" />';
+    });
 
-  _.map(this.config.list, function(item) {
-    var klass = 'pen-icon icon-' + item;
-    icons += '<i class="' + klass + '" data-action="' + item + '">' + (item.match(/^h[1-6]|p$/i) ? item.toUpperCase() : '') + '</i>';
-    if((item === 'createlink')) icons += '<input class="pen-input" placeholder="http://" />';
-  });
+    var menu = doc.createElement('div');
+    menu.setAttribute('class', this.config.class + '-menu pen-menu');
+    menu.innerHTML = icons;
+    menu.style.display = 'none';
 
-  var menu = doc.createElement('div');
-  menu.setAttribute('class', this.config.class + '-menu pen-menu');
-  menu.innerHTML = icons;
-  menu.style.display = 'none';
+    doc.body.appendChild((this._menu = menu));
 
-  doc.body.appendChild((this._menu = menu));
+    var setpos = function() {
+      if(menu.style.display === 'block') that.menu();
+    };
 
-  var setpos = function() {
-    if(menu.style.display === 'block') that.menu();
-  };
+    // change menu offset when window resize / scroll
+    window.addEventListener('resize', setpos);
+    window.addEventListener('scroll', setpos);
 
-  // change menu offset when window resize / scroll
-  window.addEventListener('resize', setpos);
-  window.addEventListener('scroll', setpos);
+    var editor = this.config.editor;
+    var show = ;
+    var toggle = function;
 
-  var editor = this.config.editor;
-  var show = _.debounce(function() {
-      var range = that._sel;
-      if(!range.isCollapsed) {
-        //show menu
-        that._range = range.getRangeAt(0);
-        that.menu().highlight();
-      } else {
-        //hide menu
-        that._menu.style.display = 'none';
-      }
-    }, 200);
-  var toggle = function(event) {
-    if(!that._isDestroyed) show();
-  };
+    editor.addEventListener('mouseup', this.toggle);
+    editor.addEventListener('keyup', this.toggle);
+    editor.addEventListener('keydown', this.linebreak);
+    menu.addEventListener('click', this.click);
 
-  // toggle toolbar on mouse select
-  editor.addEventListener('mouseup', toggle);
+    return this;
+  },
+  toggle: function(event) {
+    if(!this._isDestroyed) show();
+  },
+  click: function(e) {
+    var action = e.target.getAttribute('data-action');
 
-  var linebreak = function(event) {
+    if(!action) return;
+
+    var apply = function(value) {
+      that._sel.removeAllRanges();
+      that._sel.addRange(that._range);
+      that._actions(action, value);
+      that._range = that._sel.getRangeAt(0);
+      that.highlightItems().menu();
+    };
+
+    // create link
+    if(action === 'createlink') {
+      var input = menu.getElementsByTagName('input')[0], createlink;
+
+      input.style.display = 'block';
+      input.focus();
+
+      createlink = function(input) {
+        input.style.display = 'none';
+        if(input.value) return apply(input.value.replace(/(^\s+)|(\s+$)/g, '').replace(/^(?!http:\/\/|https:\/\/)(.*)$/, 'http://$1'));
+        action = 'unlink';
+        apply();
+      };
+
+      input.onkeypress = function(e) {
+        if(e.which === 13) return createlink(e.target);
+      };
+
+      return input.onkeypress;
+    }
+
+    apply();
+  },
+  show: _.debounce(function() {
+    var range = this._sel;
+    if(!range.isCollapsed) {
+      //show menu
+      this._range = range.getRangeAt(0);
+      this.menu().highlightItems();
+    } else {
+      //hide menu
+      this._menu.style.display = 'none';
+    }
+  }, 200),
+  linebreak: function(event) {
     if (event.which === 13) {
       var selection = window.getSelection(),
           range = selection.getRangeAt(0),
@@ -76,52 +118,82 @@ Pen.prototype.toolbar = function() {
       event.preventDefault();
       return false;
     }
+  },
+  highlightItems: function() {
+
+  },
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function highlightItems() {
+  var node = this._sel.focusNode,
+      effects = this._effectNode(node),
+      menu = this._menu,
+      linkInput = menu.querySelector('input'),
+      highlight;
+
+  // remove all highlights
+  _.map(menu.querySelectorAll('.active'), function(el) {
+    el.classList.remove('active');
+  });
+
+  if (linkInput) {
+    // display link input if createlink enabled
+    linkInput.style.display = 'none';
+    // reset link input value
+    linkInput.value = '';
   }
 
-  // prevent execCommand from adding a new block level element
-  // and instead just add a br within this block
-  editor.addEventListener('keydown', linebreak);
+  highlight = function(str) {
+    var selector = '.icon-' + str,
+        el = menu.querySelector(selector);
+    return el && el.classList.add('active');
+  };
 
-  // toggle toolbar on key select
-  editor.addEventListener('keyup', toggle);
-
-  // toggle toolbar on key select
-  menu.addEventListener('click', function(e) {
-    var action = e.target.getAttribute('data-action');
-
-    if(!action) return;
-
-    var apply = function(value) {
-      that._sel.removeAllRanges();
-      that._sel.addRange(that._range);
-      that._actions(action, value);
-      that._range = that._sel.getRangeAt(0);
-      that.highlight().nostyle().menu();
-    };
-
-    // create link
-    if(action === 'createlink') {
-      var input = menu.getElementsByTagName('input')[0], createlink;
-
-      input.style.display = 'block';
-      input.focus();
-
-      createlink = function(input) {
-        input.style.display = 'none';
-        if(input.value) return apply(input.value.replace(/(^\s+)|(\s+$)/g, '').replace(/^(?!http:\/\/|https:\/\/)(.*)$/, 'http://$1'));
-        action = 'unlink';
-        apply();
-      };
-
-      input.onkeypress = function(e) {
-        if(e.which === 13) return createlink(e.target);
-      };
-
-      return input.onkeypress;
+  effects.forEach(function(item) {
+    var tag = item.nodeName.toLowerCase();
+    switch(tag) {
+      case 'a':
+        return (menu.querySelector('input').value = item.href), highlight('createlink');
+      case 'i':
+        return highlight('italic');
+      case 'code':
+        return highlight('codespan');
+      case 'u':
+        return highlight('underline');
+      case 'b':
+        return highlight('bold');
+      case 'ul':
+        return highlight('insertunorderedlist');
+      case 'ol':
+        return highlight('insertorderedlist');
+      default :
+        highlight(tag);
     }
-
-    apply();
   });
 
   return this;
+}
+
+function _effectNode(el, returnAsNodeName) {
+  var nodes = [];
+  while(el !== this.config.editor) {
+    if(el.nodeName.match(/(?:[pia]|\Au\z|\Ab\z|h[1-6]|code|[uo]l|li)/i)) {
+      nodes.push(returnAsNodeName ? el.nodeName.toLowerCase() : el);
+    }
+    el = el.parentNode;
+  }
+  return nodes;
 };
